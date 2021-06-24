66f03b66b8414d304a14723ba469a0befc8033f9
Anadi7078tx <80170438+Anadi7078tx@users.noreply.github.com>
2021-06-24 15:04:21 +0530

    Update HelloWorldPageTest.java

--- src/main/java/my/java/gui/HelloWorldPageTest.java
+++ src/main/java/my/java/gui/HelloWorldPageTest.java
@@ -1,53 +1,43 @@
 package my.java.gui;
 
-// import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
-// import org.junit.After;
-// import org.junit.AfterClass;
-// import org.junit.Before;
-// import org.junit.BeforeClass;
-// import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-// public class HelloWorldPageTest {
+public class HelloWorldPageTest {
 
-// 	@BeforeClass
-// 	public static void setUpBeforeClass() throws Exception {
-// 	}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
 
-// 	@AfterClass
-// 	public static void tearDownAfterClass() throws Exception {
-// 	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
 
-// 	@Before
-// 	public void setUp() throws Exception {
-// 	}
+	@Before
+	public void setUp() throws Exception {
+	}
 
-// 	@After
-// 	public void tearDown() throws Exception {
-// 	}
+	@After
+	public void tearDown() throws Exception {
+	}
 
-// 	@Test
-// 	public void testBuild() {
-//     	System.out.println("This is the testcase in this class");
-//         String str1="This is the testcase in this class";
-//         assertEquals("This is the testcase in this class", str1);
-// 	}
+	@Test
+	public void testBuild() {
+    	System.out.println("This is the testcase in this class");
+        String str1="This is the testcase in this class";
+        assertEquals("This is the testcase in this class", str1);
+	}
 	
-// 	@Test
-// 	public void testingHelloWorld() {
-// 		System.out.println( "Hello + World");
-// 	}
-// }
-
-
-
-
-import org.junit.jupiter.api.Test;
- 
-class JUnit5ExampleTest {
- 
-    @Test
-    void justAnExample() {
- 
-    }
+	@Test
+	public void testingHelloWorld() {
+		System.out.println( "Hello + World");
+	}
 }
+
+
+
